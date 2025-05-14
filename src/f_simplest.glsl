68c336b25d245abc4b83724fdbaaab1545718500
Kacper Skaza <kacper.skaza.100@gmail.com>
2025-05-14 16:53:45 +0200

    Initial commit

---
+++ src/f_simplest.glsl
@@ -0,0 +1,33 @@
+#version 330
+
+uniform sampler2D textureMap0; //globalnie
+uniform sampler2D textureMap1; //globalnie
+
+out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
+in vec4 iC;
+
+in vec4 l;
+in vec4 n;
+in vec4 v;
+in vec2 iTexCoord0; //globalnie
+in vec2 iTexCoord1; //globalnie
+
+void main(void) {
+
+
+	vec4 ml = normalize(l);
+	vec4 mn = normalize(n);
+	vec4 mv = normalize(v);
+	vec4 mr=reflect(-ml,mn); //Wektor odbity
+
+	float nl = clamp(dot(mn, ml), 0, 1); //Kosinus kąta pomiędzy wektorami n i l.
+	float rv = pow(clamp(dot(mr, mv), 0, 1), 25); // Kosinus kąta pomiędzy wektorami r i v podniesiony do 25 potęgi
+
+	vec4 metal=texture(textureMap0,iTexCoord0);
+	vec4 sky=texture(textureMap1,iTexCoord1);
+
+	vec4 kd=mix(metal, sky, 0.3);
+	vec4 ks=kd/3;
+
+	pixelColor= vec4(nl * kd.rgb, kd.a) + vec4(ks.rgb*rv, 0); //Wyliczenie modelu oświetlenia (bez ambient);
+}
